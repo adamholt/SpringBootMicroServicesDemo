84d8bc68d7a1cb25f6f6cea5c7d748e52b2db9db
adamh <adamh@DESKTOP-NC6HQ3V.lan1>
2020-01-26 02:53:36 -0500

    Initial commit of spring boot project

---
+++ src/main/java/com/plantplaces/demoplantplaces/DemoplantplacesApplication.java
@@ -0,0 +1,13 @@
+package com.plantplaces.demoplantplaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoplantplacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoplantplacesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plantplaces/demoplantplaces/DemoplantplacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantplaces.demoplantplaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoplantplacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
